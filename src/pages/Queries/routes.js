e9b0f8632531f0e25cb799a9e217bd9b76ad50fa
APFirebolt <aspper20@gmail.com>
2019-12-04 23:14:26 -0800

    Changes in page structure

--- src/pages/Queries/routes.js
+++ src/pages/Queries/routes.js
@@ -6,13 +6,53 @@ import SampleComponent from '../../components/Queries/sample';
 class QueryRoutes extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      search_text: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
+  handleChange(event) {
+    const { search_text } = this.state;
+    this.setState({search_text: event.target.value})
   }
 
   render() {
+    const { search_text } = this.state;
     return (
       <Router className="container">
-        <h2>Queries Component</h2>
+        <h2>Search for a topic here on github! {search_text}</h2>
+
+        <div className="row">
+          <div className="input-field col s4">
+            <label htmlFor="search_topic">Search for topic</label>
+          </div>
+
+          <div className="input-field col s6">
+            <input placeholder="Enter topic to search here!" name="search_topic" id="search_topic" type="text"
+                   onChange={(event) => this.handleChange(event)} className="validate" />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field col s4">
+            <label htmlFor="sort_by">Sort Search Results By</label>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field col s4">
+            <label htmlFor="order_by">Order search results</label>
+          </div>
+
+          <div className="input-field col s6">
+
+          </div>
+        </div>
+
+        <div className="col s2">
+          <button className="btn">Search</button>
+        </div>
 
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li><NavLink to="/query/sample">Query sample</NavLink></li>
--- src/pages/Users/routes.js
+++ src/pages/Users/routes.js
@@ -7,13 +7,38 @@ import SampleComponent from '../../components/Users/sample';
 class UserRoutes extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      search_user: ''
+    }
 
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { search_user } = this.state;
+    this.setState({search_user : event.target.value})
   }
 
   render() {
+    const { search_user } = this.state;
     return (
       <Router className="container">
-        <h2>User Routes Component</h2>
+        <h2>User Routes Component {search_user}</h2>
+
+        <div className="row">
+          <div className="input-field col s4">
+            <label htmlFor="search_user">Search for Github User</label>
+          </div>
+
+          <div className="input-field col s6">
+            <input placeholder="Enter user name to search here!" name="search_user" id="search_user" type="text"
+                   onChange={(event) => this.handleChange(event)} className="validate" />
+          </div>
+
+          <div className="col s2">
+            <button className="btn">Search User</button>
+          </div>
+        </div>
 
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li><NavLink to="/users/sample">Link to sample Component</NavLink></li>
